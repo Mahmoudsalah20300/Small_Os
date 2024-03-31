709180f2ffa9a41d79b23b701f0734b934f8526b
Mahmoudsalah20300 <Mahmoudsalah20300@gmail.com>
2024-03-31 18:48:17 +0200

    This is the Congiguration Layer where user can change Option

---
+++ CONFIG/UART_Lcfg.c
@@ -0,0 +1,17 @@
+/*****************************************************************
+* FILE DESCRIPTION:
+----------------------------------------------------------------
+ *  file    : UART_Lcfg.c
+ *  details : Source file for link time configurations UART Driver.
+ *****************************************************************/
+
+/******************************************
+  INCLUDES
+ *******************************************/
+#include "UART_types.h"
+#include "UART_cfg.h"
+
+/******************************************
+  Global Data
+ *******************************************/
+const UART_ConfigType UART_xConfig ={RECEIVER,CHAR_SIZE_8,ASYNCHRONOUS,NO_PARITY,ONE_STOP_BIT};
